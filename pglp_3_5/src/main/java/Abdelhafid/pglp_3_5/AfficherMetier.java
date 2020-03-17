ffd7960b3257f3e381c2ced4376a2c986f8946be
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-17 19:58:24 +0100

    implementation de la classe AfficherMetier qui s'en charge d'afficher le temp du log en utilisant slf4j

--- pglp_3_5/src/main/java/Abdelhafid/pglp_3_5/AfficherMetier.java
+++ pglp_3_5/src/main/java/Abdelhafid/pglp_3_5/AfficherMetier.java
@@ -1,5 +1,29 @@
 package Abdelhafid.pglp_3_5;
 
-public class AfficherMetier {
+import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AfficherMetier implements AffichageMetierInterface {
+	
+	final Logger log;
+
+	public AfficherMetier(Logger log) {
+		
+		this.log = LoggerFactory.getLogger(AfficherMetier.class);
+	}
+	
+	/*affichage du log*/
+	
+	public void afficherTemp() {
+		
+		System.out.println (LocalDateTime.now());
+		
+	}
+	/*affichage du log avec Slf4j*/
+    public void afficherTempSlf4j(String desc) {
+    	log.info(LocalDateTime.now()+ desc);
+		
+	}
 }
